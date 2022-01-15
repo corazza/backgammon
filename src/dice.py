af5dbba0e841a4db5173da09e951521f0a76b3cd
Jan Corazza <corazzajan@gmail.com>
2022-01-15 14:15:36 +0100

    fixed rolls and probability

--- src/dice.py
+++ src/dice.py
@@ -1,10 +1,15 @@
 import random
 import sys
 
+ROLLS_N = 6 * 6 / 2
+
 def all_rolls():
     for i in range(1, 7):
-        for j in range(1, 7):
+        for j in range(i, 7):
             yield (i, j)
+
+def disregard_order(roll):
+    return (min(roll), max(roll))
 
 def dice_roll():
     first = random.randrange(6)+1
@@ -16,7 +21,7 @@ def first_roll():
     while roll[0] == roll[1]:
         roll = dice_roll()
     return roll
-    
+
 def init():
     if len(sys.argv) > 1:
         random.seed(int(sys.argv[1]))
